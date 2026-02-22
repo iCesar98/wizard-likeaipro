12f45752ce4fb554b4303e7cd5601d2a3cd6e2a8
iCesar98 <cesarw1998@hotmail.com>
2026-02-21 17:47:35 -0800

    Primer commit wizard Like AI PRO

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
+});
